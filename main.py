6092e0f844efc5041b7f48093d835ebe1be84641
CHEN-YI HUANG <chenyee@cs.nctu.edu.tw>
2019-10-12 14:57:37 +0800

    :penguin: Validate test_loss and test_train

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ def model(W, feed_dict={}):
     return np.dot(np.Placeholder(x=feed_dict['x']), np.Variable(W=W))
 
 def known_model(feed_dict={}):
-    return np.dot(np.Placeholder(x=feed_dict['x']), np.Constant([2, 3]))
+    return np.dot(np.Placeholder(x=feed_dict['x']), np.Constant([[2],[3]]))
 
 # def mse(feed_dict={}):
     # diff = np.subtract(np.Placeholder(predicted_y=feed_dict['predicted_y']), np.Placeholder(true_y=feed_dict['true_y']))
@@ -73,36 +73,32 @@ def test_known_model():
     print(grad(known_model, 'x')(feed_dict={'x': x}))
 
 def test_loss():
-    data_size = 10
-    x = np.linspace(-7, 7, data_size)
-    x = _np.c_[_np.ones(data_size), x]
-    noise = _np.random.random((data_size))
-    theta = _np.array([2, 5])
-    y = _np.dot(x, theta) + noise
-    predict = value(known_model)(feed_dict={'x': x})
+    data_size = 100
+    X = 2 * _np.random.rand(data_size,1)
+    aug_X = _np.c_[_np.ones(data_size), X]
+    theta = _np.array([[5], [10]])
+    Y = aug_X @ theta + _np.random.randn(data_size,1)
+    predict = value(known_model)(feed_dict={'x': aug_X})
     print(predict)
-    v, l = value_and_grad(mse, 'predicted_y')(feed_dict={'predicted_y': predict, 'true_y': y})
+    v, l = value_and_grad(mse, 'predicted_y')(feed_dict={'predicted_y': predict, 'true_y': Y})
     print(v, l)
 
 def test_train():
-    data_size = 10
-    x = np.linspace(-7, 7, data_size)
-    x = _np.c_[_np.ones(data_size), x]
-    
-    noise = _np.random.random((data_size))
-    theta = _np.array([2, 5])
-    y = _np.dot(x, theta) + noise
-    y = y[:, None]
+    data_size = 100
+    X = 2 * _np.random.rand(data_size,1)
+    aug_X = _np.c_[_np.ones(data_size), X]
+    theta = _np.array([[5], [10]])
+    Y = aug_X @ theta + _np.random.randn(data_size,1)
 
     W = _np.random.random((2, 1))    
-    predicted_y, grad_value= value_and_grad(model)(W=W, feed_dict={'x': x})
-    loss_grad = grad(mse, 'predicted_y')(feed_dict={'predicted_y': predicted_y, 'true_y': y})
-    print(predicted_y.shape, y.shape, x.shape)
-    print(np.array_equal(loss_grad, predicted_y - y))
-    print(np.array_equal(x.T, grad_value['W']))
+    predicted_y, grad_value= value_and_grad(model)(W=W, feed_dict={'x': aug_X})
+    loss_grad = grad(mse, 'predicted_y')(feed_dict={'predicted_y': predicted_y, 'true_y': Y})
+    print(predicted_y.shape, Y.shape, X.shape)
+    print(np.array_equal(loss_grad, predicted_y - Y))
+    print(np.array_equal(aug_X.T, grad_value['W']))
     
 
 if __name__ == "__main__":
     # print(grad(test2)(x=1, y=2, z=0))
     # print(grad(test)(z=2, y=-4, x=3, w=-1))
-    train_model()
+    test_train()
